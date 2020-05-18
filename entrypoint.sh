7b0aee0cda67a76ae49f3cb78ce19af4294f0c50
Denis Žoljom <denis.zoljom@gmail.com>
2020-05-18 18:14:08 +0200

    Core install
    Even though it's already there so this will probably fail.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 cd /usr/src/wordpress/
 
 wp config create --dbname=$1 --dbuser=$2 --dbpass=$3 --dbhost=$4 --allow-root
+wp core install --url=http://localhost:8080 --title=Test --admin_user=$5 --admin_password=$6 --admin_email=admin@local.test --skip-email
 wp plugin install woocommerce --activate --allow-root
 
 cd /github/workspace
