5922e7746c6519536e97cfc4348177141dfad4db
Denis Žoljom <denis.zoljom@gmail.com>
2020-05-19 21:08:40 +0200

    Add path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
+cd /usr/src/wordpress/
+
 wp config create --dbname=$1 --dbuser=$2 --dbpass=$3 --dbhost=$4 --allow-root
 wp core install --url=http://localhost:8080 --title=Test --admin_user=$5 --admin_password=$6 --admin_email=admin@local.test --skip-email --allow-root
 wp plugin install woocommerce --activate --allow-root
